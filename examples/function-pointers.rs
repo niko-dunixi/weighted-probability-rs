ab8df1d42608ef70c801d61d1a497c78b9e3d2c2
Paul Freakn Baker <paul.baker@forcepoint.com>
2021-03-05 13:48:58 -0700

    f

--- examples/function-pointers.rs
+++ examples/function-pointers.rs
@@ -5,7 +5,6 @@ use std::error::Error;
 use std::hash::Hash;
 use weighted_probability_rs::*;
 
-/// Represents a marble to be chosen
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 enum Marble {
     Red,
@@ -13,18 +12,24 @@ enum Marble {
     Green,
 }
 
+type MarbleProducer = fn() -> Marble;
+
+fn produce_red() -> Marble {
+    Marble::Red
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    // We initialize our aliases by supplying
-    let marble_bag = Alias::from_weighted_tuples(&[
-        WeightedTuple::new(1, Marble::Red),
-        WeightedTuple::new(2, Marble::Blue),
-        WeightedTuple::new(3, Marble::Green),
+    let marble_bag: Alias<MarbleProducer> = Alias::from_weighted_tuples(&[
+        WeightedTuple::new(1, produce_red as fn() -> Marble),
+        WeightedTuple::new(2, || Marble::Blue),
+        WeightedTuple::new(3, (|| Marble::Green) as MarbleProducer),
     ])?;
     let mut rng = thread_rng();
     let mut marble_selection_counts = HashMap::new();
     let max_iterations = 1_000_000;
     for _ in 0..max_iterations {
-        let chosen_marble = marble_bag.select(&mut rng);
+        let marble_producing_function = marble_bag.select(&mut rng);
+        let chosen_marble = marble_producing_function();
         marble_selection_counts.entry(chosen_marble).or_insert(0u64);
         marble_selection_counts.insert(chosen_marble, marble_selection_counts[&chosen_marble] + 1);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 use fraction::Fraction;
-// use rand::Rng;
+use std::error::Error;
+use std::fmt;
 
+// use rand::Rng;
 // https://docs.rs/rand/0.8.3/rand/rngs/index.html
 
+/// Represents an item that we want to chose with a given weight. The
+/// weight provided can be arbitrary, needless to say larger means more
+/// likely. This value will be normalized in relation to the other weights
+/// when provided to the `Alias::from_weighted_tuples`.
 #[derive(Debug)]
 pub struct WeightedTuple<T: Copy> {
     weight: u64,
     value: T,
 }
 
-impl <T: Copy> WeightedTuple<T> {
+impl<T: Copy> WeightedTuple<T> {
+    /// Initializes a new and immutable `WeightedTuple`
     pub fn new(weight: u64, value: T) -> WeightedTuple<T> {
         WeightedTuple {
             weight: weight,
@@ -58,23 +65,49 @@ Generation:
 */
 
 #[derive(Debug)]
-pub struct Alias<T: Copy> {   
+pub struct Alias<T: Copy> {
     probabilities: Vec<NormalizedWeightTuple<T>>,
     aliases: Vec<T>,
 }
 
-impl<T: Copy> Alias<T> {
+#[derive(Debug, Clone)]
+pub struct AliasCreationError {
+    message: String,
+}
 
-    pub fn from_weighted_tuples(items: &[WeightedTuple<T>]) -> Alias<T> {
+impl Error for AliasCreationError {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}
+
+impl fmt::Display for AliasCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // write!(f, "problem occured while creating alias: ")?;
+        write!(f, "{}", self.message)?;
+        Ok(())
+    }
+}
+
+impl<T: Copy> Alias<T> {
+    pub fn from_weighted_tuples(
+        items: &[WeightedTuple<T>],
+    ) -> Result<Alias<T>, AliasCreationError> {
         // We have to scale all the weights, which involves converting them
         // into fractions in reference to all the other weights for context.
         let count = items.len() as u64;
-        let sum = items.iter().map(|wt| wt.weight).fold(0, |total, next| total + next);
-        let normalized_weight_tuples = items.iter().map(|wt| {
-            NormalizedWeightTuple {
-                fractional_weight: Fraction::new(wt.weight * count, sum),
-                value: wt.value,
-            }
+        if count == 0 {
+            return Err(AliasCreationError {
+                message: String::from("no weighted tuples were provided"),
+            });
+        }
+        let sum = items
+            .iter()
+            .map(|wt| wt.weight)
+            .fold(0, |total, next| total + next);
+        let normalized_weight_tuples = items.iter().map(|wt| NormalizedWeightTuple {
+            fractional_weight: Fraction::new(wt.weight * count, sum),
+            value: wt.value,
         });
         // This will be our finallized results
         let mut finalized_probabilities: Vec<NormalizedWeightTuple<T>> = Vec::new();
@@ -97,7 +130,8 @@ impl<T: Copy> Alias<T> {
             let current_large_item = large_items.pop().unwrap();
             finalized_probabilities.push(current_small_item);
             finalized_aliases.push(current_large_item.value);
-            let reduced_fraction = current_large_item.fractional_weight + current_large_item.fractional_weight - one;
+            let reduced_fraction =
+                current_large_item.fractional_weight + current_large_item.fractional_weight - one;
             let reduced_large_item = NormalizedWeightTuple {
                 fractional_weight: reduced_fraction,
                 value: current_large_item.value,
@@ -117,10 +151,10 @@ impl<T: Copy> Alias<T> {
             });
         }
 
-        Alias::<T> {
+        Ok(Alias::<T> {
             probabilities: finalized_probabilities,
             aliases: finalized_aliases,
-        }
+        })
     }
 
     pub fn select(&self, rng: &mut impl rand::Rng) -> T {
@@ -140,26 +174,77 @@ impl<T: Copy> Alias<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::vec;
     use fraction::Fraction;
+    use std::vec;
+    use rand::thread_rng;
+
+    #[test]
+    fn tuples_must_be_present() {
+      let empty: &[WeightedTuple<&str>] = &[];
+      let no_tuples_alias = Alias::from_weighted_tuples(empty);
+      match no_tuples_alias {
+        Ok(_) => panic!("an empty set of tuples should fail, but didn't"),
+        Err(e) => {
+          assert_eq!(format!("{}", e), "no weighted tuples were provided");
+        },
+      }
+    }
+
+    #[test]
+    fn can_select_for_singular_item() {
+      type StringProducer = fn() -> String;
+      let alias_result = Alias::from_weighted_tuples(&[
+        WeightedTuple::new(1, (|| String::from("Paul Freakn Baker")) as StringProducer),
+      ]);
+      if let Err(e) = alias_result {
+        panic!("An error occured, but should not have: {}", e);
+      }
+      let alias = alias_result.unwrap();
+      let mut rng = thread_rng();
+      let producer: StringProducer = alias.select(&mut rng);
+      assert_eq!(String::from("Paul Freakn Baker"), producer());
+    }
+
+    #[test]
+    fn closure_properly_executed() {
+      type SideEffectingFunction = fn() -> ();
+      let mut was_mutated = false;
+      let alias_result = Alias::from_weighted_tuples(&[
+        WeightedTuple::new(1, (|| was_mutated = true) as SideEffectingFunction),
+      ]);
+      if let Err(e) = alias_result {
+        panic!("An error occured, but should not have: {}", e);
+      }
+      let alias = alias_result.unwrap();
+      let mut rng = thread_rng();
+      let has_side_effects: SideEffectingFunction = alias.select(&mut rng);
+      has_side_effects();
+      assert_eq!(was_mutated, true);
+    }
 
     #[test]
     fn getting_familiarity() {
         let tuple = WeightedTuple {
             weight: 123,
-            value: String::from("Paul Freakn Baker"),
+            value: &String::from("Paul Freakn Baker"),
         };
 
         assert_eq!(tuple.weight, 123);
-        assert_eq!(tuple.value, String::from("Paul Freakn Baker"));
-        assert_eq!(format!("{:?}", tuple), String::from("WeightedTuple { weight: 123, value: \"Paul Freakn Baker\" }"));
+        assert_eq!(tuple.value, &String::from("Paul Freakn Baker"));
+        assert_eq!(
+            format!("{:?}", tuple),
+            String::from("WeightedTuple { weight: 123, value: \"Paul Freakn Baker\" }")
+        );
     }
 
     #[test]
     fn can_we_use_fractions() {
-        let items = vec!(1u64, 2u64);
+        let items = vec![1u64, 2u64];
         let sum = items.iter().fold(0, |total, next| total + next);
-        let fractions: Vec<Fraction> = items.iter().map(|&numerator| Fraction::new(numerator, sum)).collect();
+        let fractions: Vec<Fraction> = items
+            .iter()
+            .map(|&numerator| Fraction::new(numerator, sum))
+            .collect();
         assert_eq!(fractions[0], Fraction::new(1u64, 3u64));
         assert_eq!(fractions[1], Fraction::new(2u64, 3u64));
 
